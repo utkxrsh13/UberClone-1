7b8cd9bf27e0081db98a1010f619cfc49ab81b71
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-02 00:40:47 +0530

    middle

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
+const blacklistTokenModel = require('../models/blacklistToken.model')
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -22,3 +23,37 @@ module.exports.registerUser = async (req, res, next) => {
 
   return res.status(201).json({ token, user });
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password"); //evrytime when we use user or manipulate then we will use select bcause in model password select is false
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+  const token = user.generateAuthToken();
+
+  res.cookie('token', token);
+
+  return res.status(200).json({ token, user });
+};
+
+module.exports.getUserProfile = async (req, res, next) => {
+  return res.status(201).json(req.user);
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie('token');
+  const token = res.cookie.token || req.headers.auhtorization
+
+  await blacklistTokenModel.create({token});
+
+  res.status(200).json({message: 'Logged out'});
+}
