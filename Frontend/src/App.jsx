0c9142db00af884ed84bed92c2e501048e00e79e
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-10 23:45:50 +0530

    home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
 
 function App() {
   return (
     <>
-      <h1 className='bg-red-700'>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
-  )
-}
+    <div className="p-7">
+      <form>
+        <h3 className="text-xl mb-2">What's your email</h3>
+        <input className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
+          required
+          type="email"
+          placeholder="email@example.com" />
 
-export default UserLogin
+        <h3>Enter Password</h3>
+
+        <input className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
+         type="password" 
+         required 
+         placeholder="password" />
+
+        <button className="bg-[#111] text-white font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default UserLogin;
