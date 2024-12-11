595504e7172870f85e70c60c530c2111b941f426
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-11 11:31:54 +0530

    login

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,24 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
   return (
-    <div className="p-7">
-      <form>
-        <h3 className="text-xl mb-2">What's your email</h3>
-        <input className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
-          required
-          type="email"
-          placeholder="email@example.com" />
+    <div className="h-screen p-7 flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="Uber logo"
+        />
+        <form>
+          <h3 className="text-lg font-medium mb-2">What's your email</h3>
+          <input
+            className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
+            required
+            type="email"
+            placeholder="email@example.com"
+          />
 
-        <h3>Enter Password</h3>
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
 
-        <input className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
-         type="password" 
-         required 
-         placeholder="password" />
+          <input
+            className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
+            type="password"
+            required
+            placeholder="password"
+          />
 
-        <button className="bg-[#111] text-white font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7">Login</button>
-      </form>
+          <button className="bg-[#111] text-white font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-3">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          New Here!
+          <Link to="/signup" className="text-blue-600">
+            Create new account
+          </Link>
+        </p>
+      </div>
+      <div>
+        <button className="bg-[#c1f524] text-black font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7">
+          Sign in as captain
+        </button>
+      </div>
     </div>
   );
 };
