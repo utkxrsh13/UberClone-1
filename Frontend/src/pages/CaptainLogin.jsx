2db00169f4668f8a22ce4c193a9fbaecca015ce1
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-11 18:36:09 +0530

    signup

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CaptainLogin = () => {
-  return (
-    <div>CaptainLogin</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [captainData, setCaptainData] = useState({});
 
-export default CaptainLogin
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCaptainData({
+      email: email,
+      password: password,
+    });
+    console.log(userData);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="h-screen p-7 flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-3"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_jn2qOHnlX5UNHlIIHMBXg70W1UjlJia3S01jhb6qvvIcMkQcq84D_C-MeDflKDWHRIY&usqp=CAU"
+          alt="Uber logo"
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What's your email</h3>
+          <input
+            className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            type="email"
+            placeholder="email@example.com"
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+
+          <input
+            className="bg-[#eeee] rounded px-4 py-2 w-full text-lg placeholder:text-base mb-7"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="password"
+          />
+
+          <button className="bg-[#111] text-white font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-3">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Join a fleet!
+          <Link to="/captain-signup" className="text-blue-600">
+            Register as a Captain
+          </Link>
+        </p>
+      </div>
+      <div>
+        <Link
+          to="/login"
+          className="bg-[#f5c124] flex items-center justify-center text-black font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-5"
+        >
+          Sign in as user
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainLogin;
--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UserSignup = () => {
-  return (
-    <div>UserSignup</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [userData, setUserData] = useState({});
 
-export default UserSignup
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      username: {
+        firstname: firstname,
+        lastname: lastname,
+      },
+      email: email,
+      password: password,
+    });
+    console.log(userData);
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="h-screen p-7 flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-3"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_jn2qOHnlX5UNHlIIHMBXg70W1UjlJia3S01jhb6qvvIcMkQcq84D_C-MeDflKDWHRIY&usqp=CAU"
+          alt="Uber logo"
+        />
+        <form onSubmit={(e)=>{
+          submitHandler(e);
+        }}>
+          <h3 className="text-lg font-medium mb-2">What's your name</h3>
+          <div className="flex gap-3 mb-5">
+            <input
+              className="bg-[#eeee] w-1/2 rounded px-4 py-2 text-base placeholder:text-base"
+              required
+              type="text"
+              placeholder="first name"
+              value={firstname}
+              onChange={(e) => {
+                setFirstname(e.target.value);
+              }}
+            />
+            <input
+              className="bg-[#eeee] w-1/2 rounded px-4 py-2 text-base placeholder:text-base"
+              required
+              type="text"
+              placeholder="last name"
+              value={lastname}
+              onChange={(e) => {
+                setLastname(e.target.value);
+              }}
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">What's your email</h3>
+          <input
+            className="bg-[#eeee] rounded px-4 py-2 w-full text-base placeholder:text-base mb-5"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            type="email"
+            placeholder="email@example.com"
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            className="bg-[#eeee] rounded px-4 py-2 w-full text-base placeholder:text-base mb-5"
+            type="password"
+            required
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+
+          <button className="bg-[#111] text-white font-semibold rounded px-4 py-2 w-full text-lg placeholder:text-base mb-3">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Already have aa account!
+          <Link to="/login" className="text-blue-600">
+            Login Here
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-[10px] leading-tight">
+          By proceeding, you consent to get calls, Whatsapp or SMS messages,
+          including by Automated means, From Uber and its affiliates to the
+          email provided.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserSignup;
