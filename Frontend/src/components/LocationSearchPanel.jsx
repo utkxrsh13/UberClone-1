53847e4fdc5a682a0ad0bfc6a023b1443b995ce4
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-18 23:54:06 +0530

    just

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const LocationSearchPanel = () => {
   return (
-    <div>LocationSearchPanel</div>
-  )
-}
+    <div className="">
+      <div className="flex items-center my-4 gap-3 justify-start">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-12 rounded-full">
+          <i class="ri-map-pin-fill"></i>
+        </h2>
+        <h4 className="font-medium ">
+          C6 6 Esic Colony Sarvodeya Nagar near Jk temple ,Kanpury Sarvodeya
+          ,Kanpur
+        </h4>
+      </div>
+      <div className="flex items-center my-4 gap-3 justify-start">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-12 rounded-full">
+          <i class="ri-map-pin-fill"></i>
+        </h2>
+        <h4 className="font-medium ">
+          C6 Esic 6 Esic Colony Sarvodeya Nagar near Jk temple ,Kanpureya
+          ,Kanpur
+        </h4>
+      </div>
+      <div className="flex items-center my-4 gap-3 justify-start">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-12 rounded-full">
+          <i class="ri-map-pin-fill"></i>
+        </h2>
+        <h4 className="font-medium ">
+          C6 Esic Colony 6 Esic Colony Sarvodeya Nagar near Jk temple ,Kanpur
+        </h4>
+      </div>
+      <div className="flex items-center my-4 gap-3 justify-start">
+        <h2 className="bg-[#eee] h-7 flex items-center justify-center w-12 rounded-full">
+          <i class="ri-map-pin-fill"></i>
+        </h2>
+        <h4 className="font-medium ">
+          C6 Esic Colony Sarvodeya Nagar near Jk temple ,Kanpur
+        </h4>
+      </div>
+    </div>
+  );
+};
 
-export default LocationSearchPanel
+export default LocationSearchPanel;
