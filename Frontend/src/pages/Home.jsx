e989ccba6c2f426b698be71f40cb821e26d0916a
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-18 20:12:29 +0530

    panel

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,35 +1,105 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
 
 const Home = () => {
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
 
-  const submitHnadler = ()=>{
-    
-  }
+  const submitHnadler = (e) => {
+    e.preventdefault();
+  };
+
+  useGSAP(
+    function () {
+      if (panelOpen) {
+        gsap.to(panelRef.current, {
+          height: "70%",
+          opacity:1
+        });
+        gsap.to(panelCloseRef.current,{
+          opacity:1
+        })
+      } else {
+        gsap.to(panelRef.current, {
+          height: "0%",
+          opacity:0
+        });
+        gsap.to(panelCloseRef.current,{
+          opacity:0
+        })
+      }
+    },
+    [panelOpen]
+  );
 
   return (
-    <div className='h-screen relative'>
-      <img className='absolute w-16 left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber logo" />
+    <div className="h-screen relative">
+      <img
+        className="absolute w-16 left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt="Uber logo"
+      />
 
-      <div className='h-screen w-screen'>
-        <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+      <div className="h-screen w-screen">
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
       </div>
-      <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[30%] p-5 bg-white'>
-        <h4 className='text-2xl font-semibold'>Find a trip</h4>
-        <form onSubmit={(e)=>{
-          submitHnadler(e);
-        }}>
-          <input className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5' type="text" placeholder='Add a pick-up location' />
-          <input className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3' type="text" placeholder='Enter your destination' />
-        </form>
-        </div>
-        <div className='h-[70%] bg-red-500 p-5 hidden'>
 
+      <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
+        <div className="h-[30%] p-5 bg-white relative">
+          <h5 ref={panelCloseRef} onClick={() => {
+            setPanelOpen(false)
+          }} className="absolute opacity-0 right-6 top-4 text-2xl">
+            <i className="ri-arrow-down-wide-fill"></i>
+          </h5>
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
+
+          <form
+            onSubmit={(e) => {
+              submitHnadler(e);
+            }}
+          >
+            <div className="line absolute h-20 w-1 top-[43%] left-10 bg-gray-800 rounded-full"></div>
+
+            <input
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value);
+              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a pick-up location"
+            />
+            <input
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              value={destination}
+              onChange={(e) => {
+                setDestination(e.target.value);
+              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3"
+              type="text"
+              placeholder="Enter your destination"
+            />
+          </form>
         </div>
+
+        <div ref={panelRef} className="bg-red-500 h-0 opacity-0"></div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
